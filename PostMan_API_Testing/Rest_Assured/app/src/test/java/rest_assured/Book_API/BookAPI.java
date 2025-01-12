2c02ec21ae38c3739e86c38b06fe4dd29dadf9ca
saajanvarghese <johnsaj97@gmail.com>
2025-01-12 22:01:38 +0530

    Rest Assured Code Added

---
+++ PostMan_API_Testing/Rest_Assured/app/src/test/java/rest_assured/Book_API/BookAPI.java
@@ -0,0 +1,133 @@
+package rest_assured.Book_API;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.response.*;
+
+import java.util.HashMap;
+
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+public class BookAPI {
+
+    static String author = "J.K. Rowling";
+    static String category = "Fantasy";
+    static double price = 19.99;
+    static String title = "Harry Potter and the Chamber of Secrets";
+
+    HashMap<String,Object> data = new HashMap<String,Object>();
+
+    @Test(priority = 1)
+     public void createBook(ITestContext context){
+        
+        // HashMap "data" initialized as global variable
+
+        data.put("author", author); // "author" declared as global variable
+        data.put("category",category); // "category" declared as global variable
+        data.put("price", price); // "price" declared as global variable
+        data.put("title", title); // "title" declared as global variable
+
+       String id = given()
+        .contentType("application/json")
+        .body(data)
+
+        .when()
+        .post("http://localhost:3000/books")
+        .jsonPath().getString("id");
+
+        System.out.println("Generated id is "+id);
+
+        context.setAttribute("id", id);
+    }
+
+    @Test(priority = 2)
+    public void getBook(ITestContext context){
+
+        String id = (String) context.getAttribute("id");
+
+        Response response = given()
+        .pathParam("id", id)
+
+        .when()
+        .get("http://localhost:3000/books/{id}");
+
+
+        response.then()
+        .statusCode(200)
+        .log().all();
+
+        String response_id = response.jsonPath().getString("id");
+        String response_author = response.jsonPath().getString("author");
+        String response_category = response.jsonPath().getString("category");
+        double response_price = response.jsonPath().getDouble("price");
+        String response_title = response.jsonPath().getString("title");
+
+        Assert.assertEquals(id, response_id);
+        Assert.assertEquals(author, response_author);
+        Assert.assertEquals(category, response_category);
+        Assert.assertEquals(price, response_price);
+        Assert.assertEquals(title, response_title);
+    }    
+
+    @Test(priority = 3)
+    public void updateBook(ITestContext context){
+        String id = (String) context.getAttribute("id");
+
+        String update_title = "Harry Potter and the Prisioner of Azkaban";
+        double update_price = 22.00;
+        
+        data.replace("title", update_title);
+        data.replace("price", update_price);
+
+        Response response =given()
+        .contentType("application/json")
+        .pathParam("id", id)
+        .body(data)
+
+        .when()
+        .put("http://localhost:3000/books/{id}");
+
+        response.then()
+        .statusCode(200)
+        .log().body();
+
+        String response_id = response.jsonPath().getString("id");
+        double updated_price = response.jsonPath().getDouble("price");
+        String updated_title = response.jsonPath().getString("title");
+
+        Assert.assertEquals(id, response_id);
+        Assert.assertNotEquals(price, updated_price);
+        Assert.assertNotEquals(title, updated_title);
+    }
+
+    @Test(priority = 4)
+    public void deleteBook(ITestContext context){
+        String id = (String) context.getAttribute("id");
+
+        given()
+        .pathParam("id", id)
+
+        .when()
+        .delete("http://localhost:3000/books/{id}")
+
+        .then()
+        .statusCode(200);
+    }
+
+    @Test(priority = 5)
+    public void getStoredBook(ITestContext context){
+        String id = (String) context.getAttribute("id");
+        
+        given()
+        .pathParam("id", id)
+
+        .when()
+        .get("http://localhost:3000/books/{id}")
+
+        .then()
+        .statusCode(404)
+        .log().all();
+    }
+}
